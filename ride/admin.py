ef8a8ef2eec3b3101a3393a3ea6e324381f9c02a
deima24 <12deima24@gmail.com>
2023-03-15 21:17:58 +0000

    Created templates

--- ride/admin.py
+++ ride/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    #Creates slug name from the title
+    prepopulated_fields = {'slug': ('title',)}
+
+    #adds filter to see status of the post and date
+    list_filter = ['status', 'created_on']
+
+    #Shows the title , slug , status and when the post was created
+    list_display = ('title', 'slug', 'status', 'created_on')
+
+    #Adds a search field so you can search post by the title or content
+    search_fields = ('title', 'content')
+    summernote_fields = ('content')
+
+# Creating comment admin page to manage it
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email address', 'body')
+    actions = ['approve_comments']
+
+    # Post request to be approved
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- ride/views.py
+++ ride/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filters(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    #if there is more then 6 then django add page navigation
+    paginate_by = 6
