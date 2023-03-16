fc234a9ddca5c3ab50b097a571dd37a9519ac986
deima24 <12deima24@gmail.com>
2023-03-16 21:34:15 +0000

    created templates

--- bikeride/urls.py
+++ bikeride/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("ride.urls"), name="ride-urls"),
     path('summernote/', include('django_summernote.urls')),
 ]
--- ride/views.py
+++ ride/views.py
@@ -5,7 +5,8 @@ from .models import Post
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filters(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
     #if there is more then 6 then django add page navigation
     paginate_by = 6
+
