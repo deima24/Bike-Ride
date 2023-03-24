caa55cdd1885ea51bdf7f8bfd41c236502d37e2e
deima24 <12deima24@gmail.com>
2023-03-24 23:49:32 +0000

    Posted first post

--- bikeride/urls.py
+++ bikeride/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path("", include("ride.urls"), name="ride_urls"),
+    path("", TemplateView.as_view(template_name="index.html"),  name="home"),
+    path("posts/", include("ride.urls"), name="ride_urls"),
 ]
--- ride/views.py
+++ ride/views.py
@@ -6,7 +6,7 @@ from .models import Post
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "index.html"
+    template_name = "rides.html"
     #if there is more then 6 then django add page navigation
     paginate_by = 6
 
@@ -17,6 +17,7 @@ class PostDetail(View):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
