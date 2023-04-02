651934b11ac69da53a82256aa16b4daf0196af46
deima24 <12deima24@gmail.com>
2023-04-02 23:05:05 +0000

    created comment section with a form to post a comment

--- bikeride/settings.py
+++ bikeride/settings.py
@@ -52,12 +52,15 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
 ]
 
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
